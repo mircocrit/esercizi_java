cdb27c059632c2d01a68418ee30ce0dae57ef65c
arkango <saracinoarcangelo@gmail.com>
2019-06-02 09:22:14 +0200

    array init sample

--- array_init.java
+++ array_init.java
@@ -3,5 +3,19 @@
 */
 
 public class array_init{
-    
+    public static void main(String[] args) {
+        Integer[] a = {
+            new Integer(1),
+            new Integer(2),
+            new Integer(3)
+        };
+
+        Integer[] b = new Integer[] {
+            new Integer(1),
+            new Integer(2),
+            new Integer(3)
+        };
+
+        
+    }
 }
