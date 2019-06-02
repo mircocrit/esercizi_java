3b1d588298b2e3ba0824b3a4ffa32221032f488e
arkango <saracinoarcangelo@gmail.com>
2019-06-02 19:35:25 +0200

    overloading invece che upcasting

--- Music2.java
+++ Music2.java
@@ -9,3 +9,46 @@ class Note {
     B_FLAT = new Note(0);
 }
 
+class Instrument {
+    public void play(Note n){
+        System.out.println("Instrument.play()");
+    }
+}
+
+class Wind extends Instrument{
+    public void play(Note n){
+        System.out.println("Wind.play()");  
+    }
+}
+
+class Stringed extends Instrument{
+    public void play(Note n){
+        System.out.println("Stringed.play()");  
+    } 
+}
+
+class Brass extends Instrument{
+    public void play(Note n){
+        System.out.println("Brass.play()");  
+    }     
+}
+
+public class Music2{
+    public static void tune(Wind i){
+        i.play(Note.MIDDLE_C);
+    }
+    public static void tune(Stringed i){
+        i.play(Note.MIDDLE_C);
+    }
+    public static void tune(Brass i){
+        i.play(Note.MIDDLE_C);
+    }
+    public static void main(String[] args){
+        Wind flute = new Wind();
+        Stringed violin = new Stringed();
+        Brass frenchHorn = new Brass();
+        tune(flute); // no upcasting
+        tune(violin); // no upcasting
+        tune(frenchHorn); // no upcasting
+    }
+}
