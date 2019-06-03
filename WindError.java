5536ffd2537a16037a3ee8c0b2f45c64201b0ae5
arkango <saracinoarcangelo@gmail.com>
2019-06-03 09:12:29 +0200

    overriding vs overloading

---
+++ WindError.java
@@ -0,0 +1,32 @@
+/**
+ * overriding vs overloading
+ * 
+ * Accindetaly changing the interface
+ */
+
+ class NoteX{
+     public static final int MIDDLE_C = 0,C_SHARP = 1, C_FLAT = 2;
+ }
+
+ class InstrumentX{
+     public void play(int NoteX){
+         System.out.println("InstrumentX.play()");
+     }
+ }
+
+ class WindX extends InstrumentX{
+     //OOPS! modifica l'interfaccia del metodo
+     public void play(NoteX n){
+        System.out.println("WindX.play(NoteX n)");
+     }
+ }
+
+ public class WindError{
+     public static void tune(InstrumentX i){
+         i.play(NoteX.MIDDLE_C);
+     }
+     public static void main(String[] args){
+         WindX flute = new WindX();
+         tune(flute);
+     }
+ }
