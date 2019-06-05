3cd955c540f410a804c44e9d6bfa78791871a119
arkango <saracinoarcangelo@gmail.com>
2019-06-05 07:50:38 +0200

    Eccezione personalizzata

---
+++ SimpleExceptionDemo.java
@@ -0,0 +1,18 @@
+/**
+ * Eccezione personalizzata
+ */
+class SimpleException extends Exception {}
+public class SimpleExceptionDemo {
+    public void f() throws SimpleException {
+        System.out.println("Throwing SimpleException from f()");
+        throw new SimpleException();
+    }
+    public static void main(String[] args){
+        SimpleExceptionDemo sed = new SimpleExceptionDemo();
+        try{
+            sed.f();
+        }catch(SimpleException e){
+            System.err.println("Caught it!");
+        }
+    }
+}
