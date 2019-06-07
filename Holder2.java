705fb3751acbb7ba7f230b0901a08a46e1c93026
arkango <saracinoarcangelo@gmail.com>
2019-06-07 08:01:56 +0200

    esempio di holder di qualsiasi oggetto

---
+++ Holder2.java
@@ -0,0 +1,29 @@
+/**
+ * Esempio di java generics
+ */
+
+ public class Holder2{
+     private Object a;
+     public Holder2(Object a){
+         this.a = a;
+     }
+ 
+     public void set(Object a){
+         this.a = a;
+     }
+
+     public Object get(){ return a;}
+ 
+     public static void main(String[] args){
+         Holder2 h2 = new Holder2(new Integer(1));
+         Integer i = (Integer)h2.get();
+         h2.set("string obj");
+         String s = (String)h2.get();
+         h2.set(1);
+         //Autoboxes to Integer
+         Integer k = (Integer)h2.get();
+     }
+ 
+ 
+ 
+ }
