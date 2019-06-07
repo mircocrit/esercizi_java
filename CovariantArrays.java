875bd62bf913b59199c54b2a30fd704b7b2b57dd
arkango <saracinoarcangelo@gmail.com>
2019-06-07 14:40:34 +0200

    array con tipo definito non possono incglobare altri tipi

---
+++ CovariantArrays.java
@@ -0,0 +1,22 @@
+class Fruit {}
+class Apple extends Fruit {}
+class Jonathan extends Apple {}
+class Orange extends Fruit {}
+
+public class CovariantArrays {
+  public static void main(String[] args) {
+    Fruit[] fruit = new Apple[10];
+    fruit[0] = new Apple(); // OK
+    fruit[1] = new Jonathan(); // OK
+    // Runtime type is Apple[], not Fruit[] or Orange[]:
+    try {
+      // Compiler allows you to add Fruit:
+      fruit[0] = new Fruit(); // ArrayStoreException
+		//errore a run-time, ma non a compile-time
+    } catch(Exception e) { System.out.println(e); }
+    try {
+      // Compiler allows you to add Oranges:
+      fruit[0] = new Orange(); // ArrayStoreException
+    } catch(Exception e) { System.out.println(e); }
+  }
+} 
